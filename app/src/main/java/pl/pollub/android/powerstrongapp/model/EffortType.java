63c253449eab7bcb1d3103fe970454ecaa7cbcaf
Bartosz Maksymilian Klimiuk <bartekklimiuk@gmail.com>
2025-10-29 19:34:44 +0100

    Fix: Updated EffortType and ExecutedSet

--- app/src/main/java/pl/pollub/android/powerstrongapp/model/EffortType.java
+++ app/src/main/java/pl/pollub/android/powerstrongapp/model/EffortType.java
@@ -8,4 +8,5 @@ public class EffortType {
     @PrimaryKey
     public int id;
     public String effortTypeName;
+    public String description;
 }
--- app/src/main/java/pl/pollub/android/powerstrongapp/model/ExecutedSet.java
+++ app/src/main/java/pl/pollub/android/powerstrongapp/model/ExecutedSet.java
@@ -4,21 +4,28 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
+import java.util.Date;
+
 @Entity(
         tableName = "executed_sets",
         foreignKeys = {
                 @ForeignKey(entity = Exercise.class, parentColumns = "id", childColumns = "exercise_id"),
-                @ForeignKey(entity = TrainingDay.class, parentColumns = "id", childColumns = "training_day_id")
+                @ForeignKey(entity = TrainingDay.class, parentColumns = "id", childColumns = "training_day_id"),
+                @ForeignKey(entity = UsersTrainingPlans.class, parentColumns = "user_id, training_plan_id", childColumns = "user_id, training_plan_id")
         }
 )
+
 public class ExecutedSet {
     @PrimaryKey(autoGenerate = true)
     public int id;
+    public int setNumber;
     public int executedRepetitions;
     public float weightUsed;
     public float weightPlanned;
+    public Date executionDate;
     public int exercise_id;
     public int training_day_id;
     public int effort_type_id;
     public int user_id;
+    public int training_plan_id;
 }

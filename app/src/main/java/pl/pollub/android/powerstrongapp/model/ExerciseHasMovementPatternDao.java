e3e741be281498d8cfa32d6acc5879238328da3e
Bartosz Maksymilian Klimiuk <bartekklimiuk@gmail.com>
2025-11-04 12:17:22 +0100

    Fix: Updated ExerciseHasMovementPatternDao (#PowerStrong)

--- app/src/main/java/pl/pollub/android/powerstrongapp/model/ExerciseHasMovementPatternDao.java
+++ app/src/main/java/pl/pollub/android/powerstrongapp/model/ExerciseHasMovementPatternDao.java
@@ -1,4 +1,27 @@
 package pl.pollub.android.powerstrongapp.model;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import java.util.List;
+
+@Dao
 public interface ExerciseHasMovementPatternDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(ExerciseHasMovementPattern relation);
+
+    @Delete
+    void delete(ExerciseHasMovementPattern relation);
+
+    @Query("SELECT * FROM exercise_has_movement_pattern WHERE exercise_id = :exerciseId")
+    List<ExerciseHasMovementPattern> getMovementPatternsIdForExerciseId(int exerciseId);
+
+    @Transaction
+    @Query("SELECT mp.* FROM movement_pattern mp INNER JOIN exercise_has_movement_pattern ehmp ON mp.id = ehmp.movement_pattern_id WHERE ehmp.exercise_id = :exerciseId")
+    List<MovementPattern> getMovementPatternsForExerciseId(int exerciseId);
 }
+

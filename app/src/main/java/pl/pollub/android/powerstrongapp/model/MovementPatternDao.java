0aec43a62cae73d496e10f0d95036b458551b5ab
Bartosz Maksymilian Klimiuk <bartekklimiuk@gmail.com>
2025-11-07 19:14:20 +0100

    Fix: Updated MovementPatternDao (#PowerStrong)

--- app/src/main/java/pl/pollub/android/powerstrongapp/model/MovementPatternDao.java
+++ app/src/main/java/pl/pollub/android/powerstrongapp/model/MovementPatternDao.java
@@ -16,4 +16,10 @@ public interface MovementPatternDao {
 
     @Query("SELECT * FROM movement_pattern WHERE id = :id")
     List<MovementPattern> getMovementPatternById(int id);
+
+    @Query("SELECT mp.* FROM movement_pattern mp INNER JOIN exercise_has_movement_pattern ehmp ON mp.id = ehmp.movement_pattern_id INNER JOIN exercise e ON ehmp.exercise_id = e.id WHERE e.id = :exerciseId")
+    List<MovementPattern> getMovementPatternsByExerciseId(int exerciseId);
+
+    @Query("SELECT e.* FROM exercise e INNER JOIN exercise_has_movement_pattern ehmp ON e.id = ehmp.exercise_id INNER JOIN movement_pattern mp ON ehmp.movement_pattern_id = mp.id WHERE mp.id = :movementPatternId")
+    List<Exercise> getExercisesByMovementPatternId(int movementPatternId);
 }

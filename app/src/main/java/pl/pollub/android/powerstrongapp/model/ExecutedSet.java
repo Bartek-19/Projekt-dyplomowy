f8c96f9f36952d5a5704af11ab59a38e602445e7
Bartosz Maksymilian Klimiuk <bartekklimiuk@gmail.com>
2025-11-09 20:01:12 +0100

    Fix: Updated ExecutedSet (#PowerStrong)

--- app/src/main/java/pl/pollub/android/powerstrongapp/model/ExecutedSet.java
+++ app/src/main/java/pl/pollub/android/powerstrongapp/model/ExecutedSet.java
@@ -4,12 +4,9 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Date;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
@@ -18,8 +15,8 @@ import lombok.Setter;
         tableName = "executed_set",
         foreignKeys = {
                 @ForeignKey(entity = PlannedExercise.class, parentColumns = "id", childColumns = "planned_exercise_id"),
-                @ForeignKey(entity = UserTrainingPlan.class, parentColumns = "user_id, training_plan_id",
-                        childColumns = "user_training_plan_user_id, user_training_plan_plan_id")
+                @ForeignKey(entity = UserTrainingPlan.class, parentColumns = {"user_id", "training_plan_id"},
+                        childColumns = {"user_training_plan_user_id", "user_training_plan_plan_id"})
         }
 )
 public class ExecutedSet {
